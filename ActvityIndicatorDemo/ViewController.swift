55ddaa410d22fbfe93f071980a222740e899ee17
Kushal <kushal29june@gmail.com>
2022-07-22 12:31:38 +0530

    Activity Indicator added.

--- ActvityIndicatorDemo/ViewController.swift
+++ ActvityIndicatorDemo/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        activateActivityIndicator()
+        
     }
 
 
+    
+    
+    func activateActivityIndicator(){
+    
+        indicator.style = .large
+        
+                
+        
+    
+    }
+    
+    
+    @IBAction func startaAnimating(_ sender: Any) {
+        indicator.startAnimating()
+    }
+    
+
+    @IBAction func stopAnimating(_ sender: Any) {
+        indicator.stopAnimating()
+    }
 }
 
